d2c24166bfd57902d1b95eceab815b8f84ee4f10
Behnoosh <behnoosh.beheshti@gmail.com>
2021-04-22 17:57:08 +0200

    Add basic Css

--- src/Weather.js
+++ src/Weather.js
@@ -6,15 +6,27 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-9">
+          <div className="col-6">
             <input
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Current"
+              className="btn btn-success w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,32 +37,39 @@ export default function Weather() {
         </li>
         <li>Rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-            alt="Rain"
-          />
-          <strong>2</strong>
-          <span>
-            <a href="/">°C</a>|<a href="/">°F</a>
-          </span>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+              alt="Rain"
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong className="temperature">2</strong>
+              <span className="units">
+                <a href="/">°C</a>|<a href="/">°F</a>
+              </span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
-          <ul>
-            <li>
-              Feels like: <span>1</span>
-            </li>
-            <li>
-              Visibility: <span>20</span>%
-            </li>
-            <li>
-              Humidity: <span>94</span>%
-            </li>
-            <li>
-              Wind: <span>6</span>km/h
-            </li>
-          </ul>
+          <div className="parameters">
+            <ul>
+              <li>
+                Feels like: <span>1</span>
+              </li>
+              <li>
+                Visibility: <span>20</span>%
+              </li>
+              <li>
+                Humidity: <span>94</span>%
+              </li>
+              <li>
+                Wind: <span>6</span>km/h
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
