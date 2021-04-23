d1f30b9607c0586cd92c2ab243430339cbc71c6a
Behnoosh <behnoosh.beheshti@gmail.com>
2021-04-23 14:25:49 +0200

    Add icon animated

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       visibility: response.data.visibility / 100,
       humidity: response.data.main.humidity,
       wind: Math.round(response.data.wind.speed),
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
