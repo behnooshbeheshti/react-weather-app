a8b02c1a46ebc59747f183c9336503086b8551c2
Behnoosh <behnoosh.beheshti@gmail.com>
2021-04-23 16:36:53 +0200

    Update footer links

--- src/App.js
+++ src/App.js
@@ -8,12 +8,21 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="stockholm" />
         <footer>
+          Open-sourced on{" "}
           <a
             href="https://github.com/behnooshbeheshti/react-weather-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <strong>Open-source code</strong>
+            <strong>GitHub</strong>
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://frosty-allen-dcad32.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>Netlify</strong>
           </a>{" "}
           by{" "}
           <a
