2556494648745a90a2f46ce7334aabc91cd27065
Behnoosh <behnoosh.beheshti@gmail.com>
2021-04-25 18:56:47 +0200

    Update WeatherForecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,6 +26,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
